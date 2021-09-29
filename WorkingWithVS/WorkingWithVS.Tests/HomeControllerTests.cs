3690692a71e2996d149092736d9bb468c14bef31
YoBDa <niksiban@gmail.com>
2021-09-29 20:56:47 +0300

    Added working-with-vs project

---
+++ WorkingWithVS/WorkingWithVS.Tests/HomeControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WorkingWithVS.Controllers;
+using WorkingWithVS.Models;
+using Xunit;
+using Moq;
+
+namespace WorkingWithVS.Tests
+{
+    public class HomeControllerTests
+    {
+
+        class ModelCompleteFakeRepository : IRepository
+        {
+            public IEnumerable<Product> Products { get; set; }
+
+            public void AddProduct(Product p)
+            {
+                return;
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(ProductTestData))]
+        public void IndexActionModelComplete(Product[] products)
+        {
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(products);
+            var controller = new HomeController { Repository = mock.Object };
+       
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+    }
+}
---
+++ WorkingWithVS/WorkingWithVS/Models/SimpleRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithVS.Models
+{
+    public class SimpleRepository : IRepository
+    {
+        private static SimpleRepository sharedRepository = new SimpleRepository();
+        private Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+        public static SimpleRepository SharedRepository => sharedRepository;
+        public SimpleRepository()
+        {
+            var initialItems = new[]
+            {
+                new Product{ Name = "Kayak", Price = 275M},
+                new Product{ Name = "Lifejacket", Price = 48.95M},
+                new Product{ Name = "Soccer ball", Price = 19.50M},
+                new Product{ Name = "Corner flag", Price = 34.95M}
+
+            };
+            foreach (var item in initialItems)
+                AddProduct(item);
+                
+        }
+        public void AddProduct(Product p) => products.Add(p.Name, p);
+        public IEnumerable<Product> Products => products.Values;
+    }
+}
